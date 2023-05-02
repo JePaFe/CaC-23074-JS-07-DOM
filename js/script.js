d91d31995808cb89ca6b49165fd132358ec55432
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-05-02 17:07:11 +0200

    Agregamos borrar tarea

--- js/script.js
+++ js/script.js
@@ -30,15 +30,45 @@ const renderTareas = () => {
     (tarea) =>
       (tbody.innerHTML += `
     <tr>
-        <td>${tarea.text}</td>
+        <td class="${tarea.complete ? "complete" : ""}">${tarea.text}</td>
         <td>
-            <button type="button">Completar</button>
+            <button type="button" data-id="${
+              tarea.id
+            }" class="btn-complete">Completar</button>
             <button type="button">Editar</button>
-            <button type="button">Borrar</button>
+            <button type="button" onclick="borrarTarea(${
+              tarea.id
+            })">Borrar</button>
         </td>
     </tr>
   `)
   );
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-complete")) {
+    completarTarea(e.target.dataset.id);
+  }
+});
+
+const completarTarea = (id) => {
+  tareas.forEach((tarea) => {
+    if (tarea.id == id) {
+      tarea.complete = !tarea.complete;
+    }
+  });
+
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+
+  renderTareas();
+};
+
+const borrarTarea = (id) => {
+  if (confirm("¿Esta seguro?")) {
+    const filtradas = tareas.filter((tarea) => tarea.id != id);
+    localStorage.setItem("tareas", JSON.stringify(filtradas));
+    renderTareas();
+  }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
